fcc2099d04ca7ed583b17c11878bed2918fcbb5d
didgk0823 <didgk0823@gmail.com>
2023-07-24 05:11:49 +0900

    ADD LogIn Feature

---
+++ Clone_Chrome/app.js
@@ -0,0 +1,15 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const link = documnet.querySelector("a");
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    console.log(loginInput.value);
+}
+
+function onLinkClick(event) {
+    event.preventDefault();
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", onLinkClick);
