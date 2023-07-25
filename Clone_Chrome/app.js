8b106fd0c2e8684cc31c9e363c5a49a82f4af530
didgk0823 <didgk0823@gmail.com>
2023-07-26 05:14:12 +0900

    Add feature : Background image

--- Clone_Chrome/app.js
+++ Clone_Chrome/app.js
@@ -92,3 +92,16 @@ const showQuotes = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = showQuotes.quote;
 author.innerText = showQuotes.author;
+
+const imgs = [
+  "0.jpg",
+  "1.jpg",
+  "2.jpg"
+]
+
+const showImage = imgs[Math.floor(Math.random() * imgs.length)];
+const img = document.createElement("img");
+
+img.src = `img/${showImage}`;
+
+document.body.appendChild(img);
