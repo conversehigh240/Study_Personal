8c90366eb40c86cae991bb54e37d136e6cad2543
didgk0823 <didgk0823@gmail.com>
2023-07-26 03:00:56 +0900

    Add feature : saving and loading username

--- Clone_Chrome/app.js
+++ Clone_Chrome/app.js
@@ -1,15 +1,29 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = documnet.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     event.preventDefault();
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(username);
 }
 
-function onLinkClick(event) {
-    event.preventDefault();
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
-link.addEventListener("click", onLinkClick);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername == null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}
+else {
+    paintGreetings(savedUsername);
+}
